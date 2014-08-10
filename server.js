1d08b1359597f8fbbb87a644fdd8bbadbd0e423d
jkang107 <jkang107@gmail.com>
2014-08-10 19:28:01 +0900

    add server side code

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+var express = require("express");
+var connect = require('connect');
+var logfmt = require("logfmt");
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+var fs = require('fs');
+var file = __dirname + '/itinerary.json';
+
+app.use(logfmt.requestLogger());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+app.get('/read', function(req, res) {
+	fs.readFile(file, 'utf8', function(err, data) {
+		if (err) {
+			console.log("error: " + err);
+			return;
+		}
+		res.send(data);
+	});
+});
+
+// TODO: write file implementation
+app.post('/write', function(req, res) {
+	console.log(req.body);
+	
+	fs.writeFile(file, JSON.stringify(req.body), function(err, data) {
+		if (err) {
+			console.log("error: " + err);
+			return;
+		}
+		console.log("file written");
+	});
+	
+	
+	res.send(req.body);
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});

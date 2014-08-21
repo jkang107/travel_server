72cb6105dd874d71e8b6d062cf64e4c8d3bbdf07
jkang107 <jkang107@gmail.com>
2014-08-21 22:16:40 +0900

    add message function

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 var fs = require('fs');
 var file = __dirname + '/itinerary.json';
 
+
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -38,7 +39,27 @@ app.post('/write', function(req, res) {
 	
 	res.send(req.body);
 });
+var messageFile = __dirname + '/messages.json';
+app.post('/sendMessage', function(req, res) {
+	fs.appendFile(messageFile, JSON.stringify(req.body), function(err, data) {
+		if (err) {
+			console.log("error: " + err);
+			return;
+		}
+		console.log("message file written");
+	});
+	res.send("success send Message!");
+});
 
+app.get('/getMessages', function(req, res) {
+	fs.readFile(messageFile, 'utf8', function(err, data) {
+		if (err) {
+			console.log("error: " + err);
+			return;
+		}
+		res.send(data);
+	});
+});
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
 	console.log("Listening on " + port);
